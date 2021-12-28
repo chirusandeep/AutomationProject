5395e188a1245167de62c54bedbf692ebb45c208
Chiru Sandeep Paleti <32004793+chirusandeep@users.noreply.github.com>
2021-12-28 22:30:38 +0530

    Update OrderFlowPages.java

--- src/test/java/pages/OrderFlowPages.java
+++ src/test/java/pages/OrderFlowPages.java
@@ -45,7 +45,7 @@ public class OrderFlowPages {
 	public OrderFlowPages(WebDriver driver) {
 		this.driver = driver;
         PageFactory.initElements(driver, this);
-        this.wait = new WebDriverWait(driver, Duration.ofSeconds(5));
+        this.wait = new WebDriverWait(driver, Duration.ofSeconds(60));
 	}
 	
 	public void Tshirts() {
